453d8188228f91892261f4efc6538a25a1be7731
abelal83 <abelal83@users.noreply.github.com>
2017-12-25 20:54:56 +0000

    added some pester tests

---
+++ Tests/PSSlackBot/Remove-CompletedWorkerBot.Tests.ps1
@@ -0,0 +1,19 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\..\..\PSSlackBot\Public\$sut"
+
+Describe "Remove-CompletedWorkerBot" {
+           
+    $AsyncObject = New-Object System.Collections.Specialized.OrderedDictionary 
+
+    It "Should not throw when AsyncObject has no items" {
+        { Remove-CompletedWorkerBot -AsyncObject $AsyncObject } | Should -Not -Throw
+    }
+
+    It "Should throw when AsyncObject has item with no process" {
+
+        $AsyncObject.Add('123', @{State = @{IsCompleted = $false}; Powershell = $null; StartTime = (Get-Date).AddMinutes(-20)})
+
+        { Remove-CompletedWorkerBot -AsyncObject $AsyncObject } | Should -Throw
+    }
+}
---
+++ Tests/PSSlackBot/Start-WorkerBot.Tests.ps1
@@ -0,0 +1,30 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\..\..\PSSlackBot\Public\$sut"
+
+Describe "Start-WorkerBot" {
+
+    function New-PSBotRunspacePool
+    {
+        $sessionState = [System.Management.Automation.Runspaces.InitialSessionState]::CreateDefault2()
+        $runspacePool = [runspacefactory]::CreateRunspacePool(1, [Environment]::ProcessorCount * 3)
+        #$runspacePool = [RunspaceFactory]::CreateRunspacePool($sessionState)
+        [void] $runspacePool.Open()
+
+        return $runspacePool
+    }
+
+    $SlackMessage = @{}
+    $SlackToken = '12345'
+    $RunspacePool = New-PSBotRunspacePool        
+    $AsyncObject = New-Object System.Collections.Specialized.OrderedDictionary
+    $SlackUsers = @{}    
+
+    It "Starts a new worker bot" {
+        { Start-WorkerBot -SlackMessage $SlackMessage -SlackToken $SlackToken `
+            -RunspacePool $RunspacePool -AsyncObject $AsyncObject `
+             -SlackUsers $SlackUsers } | Should -Not -Throw
+    }
+
+    $AsyncObject[0].Powershell.Dispose()    
+}
