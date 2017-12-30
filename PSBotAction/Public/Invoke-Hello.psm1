6112e9916d68f6a64ac48eab7c6c0aba294c1716
abelal83 <abelal83@users.noreply.github.com>
2017-12-30 22:01:46 +0000

    added another leyword to hello action

--- PSBotAction/Public/Invoke-Hello.psm1
+++ PSBotAction/Public/Invoke-Hello.psm1
@@ -3,7 +3,7 @@ function Invoke-Hello
     return @{
         Id = [Guid]::NewGuid().Guid;
         Response = "Hey what's up"
-        KeyWords = @('hey', 'hello', "what's", 'up');
+        KeyWords = @('hey', 'hello', "what's", 'up', 'yo', 'bot');
         Action = '';
         Auth = @();
         AuthOverride = @()
