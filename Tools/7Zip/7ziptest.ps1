b06c79b575d63c62ebd96a3b2aa9f1923394f64e
abelal83 <abelal83@users.noreply.github.com>
2018-01-03 21:05:16 +0000

    some test code added for in memory compression\encryption mail attachment

--- Tools/7Zip/7ziptest.ps1
+++ Tools/7Zip/7ziptest.ps1
@@ -1,4 +1,52 @@
 Remove-Module SevenZip -ErrorAction Continue
 Import-Module "$PSScriptRoot\SevenZip.psm1"
 
-Compress-TextToStream -Value "abcdefg" -ZipPassword "abc"
+$attachmentStream = Compress-TextToZipStream -Value "abcdefg" -ZipPassword "abc"
+
+function Send-PSBotMail
+{
+    [CmdletBinding()]
+    param
+    (
+        [System.IO.Stream] $AttachmentStream,
+        [String] $AttachmentFileName
+    )
+
+
+    # Create attachment
+    $contentType = New-Object Net.Mime.ContentType -Property @{
+        MediaType = [Net.Mime.MediaTypeNames+Application]::Zip
+        Name = $AttachmentFileName
+        }
+
+    $attachment = New-Object Net.Mail.Attachment $AttachmentStream, $contentType
+
+    # Add the attachment
+    $message.Attachments.Add($attachment)
+
+    # Send Mail via SmtpClient
+    $smtpClient.Send($message)
+}
+
+$contentType = New-Object Net.Mime.ContentType -Property @{
+    MediaType = [System.Net.Mime.MediaTypeNames+Text]::Plain
+    Name = "test.txt"
+    }
+
+$AttachmentStream = ConvertFrom-StringToMemoryStream -InputString '12345'
+$attachment = New-Object Net.Mail.Attachment $AttachmentStream, $contentType
+$username = "abu.belal@.com"
+$password = ""
+$msg = New-Object System.Net.Mail.MailMessage
+$msg.To.Add((New-Object System.Net.Mail.MailAddress("abu.belal@.com")));
+$msg.From = New-Object System.Net.Mail.MailAddress($username);
+$msg.Subject = "Test Office 365 Account";
+$msg.Body = "Testing email using Office 365 account.";
+$msg.IsBodyHtml = $true;
+$msg.Attachments.Add($attachment)
+$client = New-Object System.Net.Mail.SmtpClient;
+$client.Host = "smtp.outlook.com";
+$client.Credentials = New-Object System.Net.NetworkCredential($username, $password);
+$client.Port = 587; 
+$client.EnableSsl = $true;
+$client.Send($msg);
--- Tools/7Zip/SevenZip.psm1
+++ Tools/7Zip/SevenZip.psm1
@@ -1,5 +1,5 @@
 Add-Type -Path "$PSScriptRoot\SevenZipSharp.dll"
-function Compress-TextToStream
+function Compress-TextToZipStream
 {
     [CmdletBinding()]
     param
@@ -9,16 +9,45 @@ function Compress-TextToStream
     )
 
     [SevenZip.SevenZipCompressor]::SetLibraryPath("$PSScriptRoot\7z64.dll")
-    $encoder = New-Object System.Text.UnicodeEncoding
-    [byte[]] $byteString = $encoder.GetBytes($Value)
+
+    $stringMemory = ConvertFrom-StringToMemoryStream -InputString $Value
 
     $compressedStream = New-Object System.IO.MemoryStream
     $compressor = New-Object SevenZip.SevenZipCompressor
     $compressor.CompressionMethod = [SevenZip.CompressionMethod]::Lzma2
     $compressor.CompressionLevel = [SevenZip.CompressionLevel]::Normal
-    $compressor.CompressStream($byteString, $compressedStream, $ZipPassword)
+    $compressor.CompressStream($stringMemory, $compressedStream, $ZipPassword)
 
     return $compressedStream
 }
 
-Export-ModuleMember -Function Compress-TextToStream
+# thanks to https://gist.github.com/Sam-Martin/1955ac4ef3972bb9e8a8
+function ConvertFrom-StringToMemoryStream
+{
+    param
+    (
+        [parameter(Mandatory)]
+        [string]$InputString
+    )
+
+    $stream = New-Object System.IO.MemoryStream;
+    $writer = New-Object System.IO.StreamWriter($stream);
+    $writer.Write($InputString);
+    $writer.Flush();
+    return $stream
+}
+
+function ConvertFrom-StreamToString
+{
+    param
+    (
+        [parameter(Mandatory)]
+        [System.IO.MemoryStream]$inputStream
+    )
+
+    $reader = New-Object System.IO.StreamReader($inputStream);
+    $inputStream.Position = 0;
+    return $reader.ReadToEnd()
+}
+
+#Export-ModuleMember -Function Compress-TextToZipStream
